7c259473134de9d6be31824b6a385f7c1c212507
ravi9607 <ravi2018cs177abesit@gmail.com>
2021-04-11 03:01:11 +0530

    first commit

--- src/main/java/com/example/thymelefetest/ThymelefeTestApplication.java
+++ src/main/java/com/example/thymelefetest/ThymelefeTestApplication.java
@@ -2,12 +2,19 @@ package com.example.thymelefetest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ThymelefeTestApplication {
+public class ThymelefeTestApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ThymelefeTestApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder
+                                                         application) {
+        return application.sources(ThymelefeTestApplication.class);
     }
 
 }
