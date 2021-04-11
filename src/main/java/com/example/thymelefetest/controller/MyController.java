dce8a30c72dd8df5acb0cc9383494e4cf491434c
ravi9607 <ravi2018cs177abesit@gmail.com>
2021-04-11 11:30:25 +0530

    Procfile

--- src/main/java/com/example/thymelefetest/controller/MyController.java
+++ src/main/java/com/example/thymelefetest/controller/MyController.java
@@ -13,13 +13,13 @@ public class MyController { // only GET and POST request velid
     @GetMapping(value = "/about")
     public String getrequest(Model model){
 
-        model.addAttribute("name","Ravi Gupta");
-
-        model.addAttribute("Date",new Date().toString());
-
-        model.addAttribute("isActive",true);  // check conditional operater
-
-        model.addAttribute("age",17);
+//        model.addAttribute("name","Ravi Gupta");
+//
+//        model.addAttribute("Date",new Date().toString());
+//
+//        model.addAttribute("isActive",true);  // check conditional operater
+//
+//        model.addAttribute("age",17);
         return "about";
     }
 }
