def04c4771549649bef9b7ae40bd028de5e6d310
ravi9607 <ravi2018cs177abesit@gmail.com>
2021-04-11 16:16:00 +0530

    Procfile

--- src/main/java/com/example/thymelefetest/controller/MyController.java
+++ src/main/java/com/example/thymelefetest/controller/MyController.java
@@ -1,18 +1,14 @@
 package com.example.thymelefetest.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
-import java.util.List;
 
 @RestController                 // Thymeleaf used only Controller
 public class MyController { // only GET and POST request velid
 
-    @RequestMapping("/api/helloworld")
+    @RequestMapping("/about")
     public String findAll(){
         return "Hello World!";
     }
@@ -20,13 +16,13 @@ public class MyController { // only GET and POST request velid
 //    @GetMapping(value = "/about")
 //    public String getrequest(Model model){
 //
-////        model.addAttribute("name","Ravi Gupta");
-////
-////        model.addAttribute("Date",new Date().toString());
-////
-////        model.addAttribute("isActive",true);  // check conditional operater
-////
-////        model.addAttribute("age",17);
+//        model.addAttribute("name","Ravi Gupta");
+//
+//        model.addAttribute("Date",new Date().toString());
+//
+//        model.addAttribute("isActive",true);  // check conditional operater
+//
+//        model.addAttribute("age",17);
 //        return "about";
 //    }
 }
